d62d537d8e7cb1c956bea58ad6f19b925a9690cb
jaeyoung.choi <lactrious@gmail.com>
2018-11-27 08:36:24 +0900

    Add new api for auto generated plist file

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,34 @@
 import { api, ApiError } from 'api-gateway-rest-handler';
 import * as AWS from 'aws-sdk';
 import { ensureAuthorized } from './auth';
+import plist from './utils/plist';
 
 const bucketName = 'd.yyt.life';
 const cloudFrontDistributionId = 'E3J0F0HBU3676';
 const downloadUrlBase = `https://${bucketName}`;
 const maxDistributionCount = 100;
 
-export const createDistribution = api(async req => {
-  await ensureAuthorized(req.header('X-Auth-Token'));
+export const createDistribution = api(
+  async req => {
+    await ensureAuthorized(req.header('X-Auth-Token'));
 
-  const s3 = new AWS.S3();
-  const { serviceName, platform, version } = req.pathParameters;
-  if (!serviceName || !platform || !version) {
-    throw new ApiError('Invalid path parameters');
-  }
-  const key = `${serviceName}/${platform}/${version}`;
-  const signedUrl = s3.getSignedUrl('putObject', {
-    Bucket: bucketName,
-    Key: key,
-    Expires: 60 * 10,
-    ContentType: 'application/binary',
-    ACL: 'public-read',
-  });
-  return signedUrl;
-});
+    const s3 = new AWS.S3();
+    const { serviceName, platform, version } = req.pathParameters;
+    if (!serviceName || !platform || !version) {
+      throw new ApiError('Invalid path parameters');
+    }
+    const key = `${serviceName}/${platform}/${version}`;
+    const signedUrl = s3.getSignedUrl('putObject', {
+      Bucket: bucketName,
+      Key: key,
+      Expires: 60 * 10,
+      ContentType: 'application/binary',
+      ACL: 'public-read',
+    });
+    return signedUrl;
+  },
+  { contentType: 'plain/text' },
+);
 
 export const deleteDistribution = api(async req => {
   await ensureAuthorized(req.header('X-Auth-Token'));
@@ -107,12 +111,15 @@ export const listAllDistributions = api(async req => {
   };
 });
 
-export const listPlatformDistributions = api(async req => {
-  const s3 = new AWS.S3();
-  const { serviceName, platform } = req.pathParameters;
+const findPlatformDistributions = async (
+  serviceName: string,
+  platform: string,
+  count: number,
+) => {
   if (!serviceName || !platform) {
     throw new ApiError('Invalid path parameters');
   }
+  const s3 = new AWS.S3();
   const prefix = `${serviceName}/${platform}/`;
   const listing = await s3
     .listObjects({
@@ -120,11 +127,44 @@ export const listPlatformDistributions = api(async req => {
       Prefix: prefix,
     })
     .promise();
-  const count = +(req.queryStringParameters.count || `${maxDistributionCount}`);
   const keys = sortedKeys(listing.Contents).slice(0, count);
   return {
     service: serviceName,
     platform,
     versions: keys.map(key => `${downloadUrlBase}/${key}`),
   };
+};
+
+export const listPlatformDistributions = api(async req => {
+  const { serviceName, platform } = req.pathParameters;
+  const count = +(req.queryStringParameters.count || `${maxDistributionCount}`);
+  return findPlatformDistributions(serviceName, platform, count);
 });
+
+export const getPlistForLatestIos = api(
+  async req => {
+    const { packageName, semver } = req.pathParameters;
+    const serviceName = packageName.split('.')[2];
+    if (!serviceName) {
+      throw new ApiError('Invalid path parameters');
+    }
+    const projectName =
+      serviceName.charAt(0).toUpperCase() + serviceName.substr(1);
+    const distributions = await findPlatformDistributions(
+      serviceName,
+      'ios',
+      1,
+    );
+    if (distributions.versions.length === 0) {
+      throw new ApiError(`No distribution for ${packageName}`);
+    }
+
+    return plist({
+      name: projectName,
+      downloadUrl: distributions.versions[0],
+      packageName,
+      semver,
+    });
+  },
+  { contentType: 'application/xml' },
+);
